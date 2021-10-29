ce2f5e28f0a4f58c3707f0f012e13fc6e3241566
Colin Campbell <prontobol@gmail.com>
2021-10-29 14:56:31 -0500

    Fetch specific

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 window.onload = () => {
     const searchBtn = document.getElementById('search-btn');
     const searchInput = document.getElementById('search-input');
+    const resultDiv = document.getElementById('result');
 
-    searchBtn.addEventListener('click',async (e)=>{
-        console.log(searchInput.value)
-        // make AJAX request
-        const response = await fetch("/superheroes.php");
-        alert(await response.text())
+    searchBtn.addEventListener('click', async (e) => {
+        const searchVal = searchInput.value;
+        let response;
+        if (searchVal.length == 0) {
+            response = await fetch("/superheroes.php");
+            resultDiv.innerHTML = await response.text()
+        } else {
+            response = await fetch(`/superheroes.php?alias=${searchVal}`)
+            const jsonResponse = await response.json();
+            const hero = jsonResponse[0];
+            console.log(hero);
+        }
+
     })
 }
